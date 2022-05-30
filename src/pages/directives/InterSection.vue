98b21a69356987593be79aa4417237807e548e1e
dongwa <783137663@qq.com>
2022-05-31 00:16:34 +0800

    feat:  指令-intersection

--- src/pages/directives/InterSection.vue
+++ src/pages/directives/InterSection.vue
@@ -1,12 +1,32 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-  </q-page>
+  <q-scroll-area class="box">
+    <div class="empty"></div>
+    <div class="example" v-intersection.once="options">被监听的Dom</div>
+    <div class="empty"></div>
+  </q-scroll-area>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  // name: 'PageName'
-})
+<script setup>
+const options = {
+  handler: onIntersection,
+  cfg: {
+    threshold: [0, 0.25, 0.5, 1],
+  },
+};
+function onIntersection(e) {
+  console.log(e);
+}
 </script>
+<style lang="scss">
+.box {
+  width: 100%;
+  height: 300px;
+  border: 1px solid gray;
+}
+.empty {
+  height: 800px;
+}
+.example {
+  height: 100px;
+  border: 1px solid gray;
+}
+</style>
