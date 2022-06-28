0c897a6c845fc2b1005f3ba6fe188a7b4a6c351b
dongwa <783137663@qq.com>
2022-06-28 23:43:41 +0800

    feat: 演示 morph

---
+++ src/pages/directives/VMorph.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page padding>
+    <q-btn @click="start">开始变形</q-btn>
+    <div v-morph:one:arr:1000="model" id="one"></div>
+    <div v-morph:two:arr:1000="model" id="two"></div>
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+let model = ref('one');
+let arr: any = {
+  one: 'two',
+  two: 'one',
+};
+function start() {
+  model.value = arr[model.value];
+}
+</script>
+<style scoped lang="scss">
+#one {
+  width: 100px;
+  height: 100px;
+  background: blue;
+}
+#two {
+  width: 200px;
+  height: 150px;
+  background: yellow;
+}
+</style>
---
+++ src/pages/utils/UMorph.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page padding>
+    <q-btn @click="start">开始变形</q-btn>
+    <div id="one" :class="{ invisible: show }"></div>
+    <div id="two" :class="{ invisible: !show }"></div>
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import { morph } from 'quasar';
+import { ref } from 'vue';
+let show = ref(false);
+
+function start() {
+  morph({
+    to: show.value ? '#one' : '#two',
+    from: show.value ? '#two' : '#one',
+    onToggle: () => {
+      show.value = !show.value;
+    },
+    duration: 1000,
+  });
+}
+</script>
+<style scoped lang="scss">
+#one {
+  width: 100px;
+  height: 100px;
+  background: blue;
+}
+#two {
+  width: 200px;
+  height: 150px;
+  background: yellow;
+}
+</style>
