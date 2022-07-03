c66ad20f085cce8025d7ab34983010ebe2c03133
dongwa <783137663@qq.com>
2022-07-03 16:23:21 +0800

    feat: 演示V-scroll

---
+++ src/pages/directives/VScroll.vue
@@ -0,0 +1,31 @@
+<style scoped>
+.empty {
+  height: 800px;
+}
+.example {
+  height: 100px;
+  border: 1px solid gray;
+}
+.example2 {
+  margin-left: 900px;
+  height: 100px;
+  border: 1px solid gray;
+}
+.e2 {
+  width: 1600px;
+}
+</style>
+<template>
+  <q-page padding>
+    <div class="empty"></div>
+    <div class="example" v-scroll="debounce(onScroll, 100)">被监听的Dom</div>
+    <div class="example2" v-scroll="debounce(onScroll, 100)">被监听的Dom</div>
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import { debounce } from 'quasar';
+function onScroll(v: number, h: number) {
+  console.log('v:', v, 'h', h);
+}
+</script>
---
+++ src/pages/directives/VScrollFire.vue
@@ -0,0 +1,24 @@
+<style scoped>
+.empty {
+  height: 800px;
+}
+.example {
+  height: 100px;
+  border: 1px solid gray;
+}
+</style>
+<template>
+  <q-page padding>
+    <div class="empty"></div>
+    <div class="example" v-scroll-fire="debounce(onScroll, 100)">
+      被监听的Dom
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import { debounce } from 'quasar';
+function onScroll(el: HTMLElement) {
+  console.log('v:', el);
+}
+</script>
